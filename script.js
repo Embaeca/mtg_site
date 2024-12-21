2f533c4fc2d82875c8e7f4f0099260be69ca4082
Embaeca <118907653+Embaeca@users.noreply.github.com>
2024-12-21 16:26:25 -0300

    atualização de responsividade e de renoção da animação

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var descricao_da_carta = document.querySelector(".descricao_da_carta");
 var anterior = document.querySelector(".anterior");
 var proximo = document.querySelector(".proxima");
 var secao = document.querySelector(".secao");
+var ligamagic = document.querySelector("#ligamagic")
+var mtg_ofc = document.querySelector("#mtg_site_ofc")
 
 var contador = 0;
 
@@ -20,6 +22,8 @@ proximo.addEventListener("click", () => {
       descricao_da_carta.innerHTML =
         "Pantlaza é o jovem companheiro raptor de Huatli, e tão igual em estatura a outros dinossauros quanto o poeta-guerreiro é aos cidadãos do Império do Sol. Leal e inteligente, Pantlaza é infamemente independente, uma característica que ele trouxe para sua parceria com Huatli.";
       secao.style.backgroundColor = "#e6b4818e";
+      ligamagic.href = "https://www.ligamagic.com.br/?view=cards/card&show=1&card=Pantlaza,%20Sun-Favored"
+      mtg_ofc.href = "https://gatherer.wizards.com/Pages/Card/Details.aspx?name=Pantlaza%2C%20Sun-Favored"
       break;
     case 1:
       carta.src = "./imgs/o_ur-dragao.png";
@@ -28,6 +32,8 @@ proximo.addEventListener("click", () => {
       descricao_da_carta.innerHTML =
         "O Ur-Dragon é descrito como uma sombra enorme, escura e aterrorizante. É um ser que vive no meio e no meio, num lugar e em nenhum lugar: é cego. Em suas viagens pelos planos, cria tempestades com o bater de suas asas: dessas tempestades nascem outros dragões.";
       secao.style.backgroundColor = "#B785B3";
+      ligamagic.href = "https://www.ligamagic.com.br/?view=cards/card&card=The+Ur-Dragon"
+      mtg_ofc.href = "https://gatherer.wizards.com/pages/card/Details.aspx?multiverseid=435143"
       break;
     case 2:
       carta.src = "./imgs/Maha, As Plumas da Noite.jpg";
@@ -36,6 +42,8 @@ proximo.addEventListener("click", () => {
       descricao_da_carta.innerHTML =
         "Maha, As Plums da Noite, era a guardiã das sombras e protetora dos seres noturnos. Nascida da magia ancestral da floresta, suas asas negras brilhavam como o céu estrelado. Quando o necromante Kairon ameaçou o reino com seu exército de sombras, Maha usou seu poder celestial para derrotá-lo, restaurando a paz. Sua bravura se tornou lenda, e até hoje, sob a lua, seu espírito vigia aqueles que necessitam.";
       secao.style.backgroundColor = "#F7D0AF";
+      ligamagic.href = "https://www.ligamagic.com.br/?view=cards/card&card=Maha%2C+Its+Feathers+Night"
+      mtg_ofc.href = "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=668774"
       break;
     default:
       break;
